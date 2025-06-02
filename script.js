87b85c87cc23226808cdbf2972e2a1a8f7b5c07d
marqswesley <142321792+marqswesley@users.noreply.github.com>
2025-06-02 12:07:34 +0000

    ok

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
 
-const caixaPrincipal = document.querySelector("caixa-principal");
-const caixaPerguntas = document.querySelector("caixa-perguntas");
-const caixaAlternativas = document.querySelector("caixa-alternativas");
-const caixaResultado = document.querySelector("caixa-resultado");
-const textoResultado = document.querySelector("texto-resultado");
+const caixaPrincipal = document.querySelector(".caixa-principal");
+const caixaPerguntas = document.querySelector(".caixa-perguntas");
+const caixaAlternativas = document.querySelector(".caixa-alternativas");
+const caixaResultado = document.querySelector(".caixa-resultado");
+const textoResultado = document.querySelector(".texto-resultado");
 const perguntas = [
     {
         enunciado: "Pergunta 1",
